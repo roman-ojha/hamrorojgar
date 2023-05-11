dd0e6df02dbb4ed838cd223e8adece0b32677029
Roman-Ojha <razzroman98@gmail.com>
2023-05-11 17:41:22 +0545

    feat: added content of the NavBar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import appIcon from "@/assets/images/appIcon.png";
+import Image from "next/image";
 
 const NavBar = (): React.JSX.Element => {
-  return <></>;
+  return (
+    <nav>
+      <div>
+        <Image src={appIcon} alt="appIcon"></Image>
+      </div>
+      <li>
+        <ul>How it works</ul>
+        <ul>About</ul>
+        <ul>Contact</ul>
+      </li>
+      <div>
+        <button>Register</button>
+        <button>Sign in</button>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
