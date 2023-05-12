1c7c41addabb1208258905a6644ade3118669692
Roman-Ojha <razzroman98@gmail.com>
2023-05-12 15:08:47 +0545

    feat(signin page): added background & welcome svg as background

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -2,12 +2,26 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import React from "react";
+import styles from "@/styles/pages/signin.module.scss";
+import welcomeImage from "@/assets/svg/welcome.svg";
 
 const SingIn: NextPage = () => {
   return (
-    <div>
-      <h1>Sign In Page</h1>
-    </div>
+    <>
+      <Head>
+        <title>Sign In</title>
+      </Head>
+      <div className={styles.signin}>
+        <div className={styles.signin__background}>
+          <div className={styles.signin__background__image}></div>
+          <Image
+            className={styles.signin__background__welcome}
+            src={welcomeImage}
+            alt="welcome"
+          ></Image>
+        </div>
+      </div>
+    </>
   );
 };
 
