b53569887f4f5ecff5847716a33d981397795752
Roman-Ojha <razzroman98@gmail.com>
2023-05-22 11:22:50 +0545

    feat: added important folders for project

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from api.views import Citizen, Registration, Login, VacancyView, ApplyView
 
 urlpatterns = [
-    path('citizens/register', Registration.as_view(), name='citizen-register'),
-    path('citizens/login', Login.as_view(), name='citizen-login'),
-    path('vacancy/', VacancyView.as_view(), name="vacancy"),
-    path('apply/', ApplyView.as_view(), name='apply')
+    path('citizens/register/', Registration.as_view(), name='citizen-register'),
+    path('citizens/login/', Login.as_view(), name='citizen-login'),
+    path('job/vacancies/', VacancyView.as_view(), name='job-vacancies'),
+    path('job/apply/', ApplyView.as_view(), name='job-apply'),
 ]
