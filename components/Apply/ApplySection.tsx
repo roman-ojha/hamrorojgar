c8ffecaee3d772bb891be11ba9778363138afb87
Roman-Ojha <razzroman98@gmail.com>
2023-05-26 16:15:37 +0545

    feat(apply page): paplulated cv image after use select the img file

--- components/Apply/ApplySection.tsx
+++ components/Apply/ApplySection.tsx
@@ -1,23 +1,51 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "@/styles/pages/apply.module.scss";
 import { Icon } from "@iconify/react";
 
 const ApplySection = (): React.JSX.Element => {
+  const cvImageElm: React.MutableRefObject<
+    null | HTMLDivElement | HTMLImageElement
+  > = useRef(null);
+  useEffect(() => {
+    // showing citizen image on change
+    document.getElementById("cv-file")?.addEventListener("change", (e) => {
+      const target = e.target as HTMLInputElement;
+      let file: File | undefined;
+      if (target.files && target.files?.length > 0) {
+        file = target.files?.[0];
+      }
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = (event) => {
+          const imageSrc = event.target?.result;
+          const newImgElement = document.createElement("img");
+          newImgElement.setAttribute("src", imageSrc as string);
+          newImgElement.setAttribute(
+            "style",
+            "height: 40vh; object-fit: contain; border-radius: 0.8vw;"
+          );
+          cvImageElm.current?.replaceWith(newImgElement);
+        };
+        reader.readAsDataURL(file);
+      }
+    });
+  }, []);
   return (
     <section className={styles.apply__main_content__apply_section}>
       <form action="">
         <div
           className={styles.apply__main_content__apply_section__cv_container}
+          ref={cvImageElm}
         >
           <h1>CV</h1>
         </div>
-        <label htmlFor="picture-file">
+        <label htmlFor="cv-file">
           <Icon icon="ph:upload-light" />
           Upload CV
         </label>
         <input
           type="file"
-          id="picture-file"
+          id="cv-file"
           hidden
           // {...register("photo")}
           // ref={fileInput}
