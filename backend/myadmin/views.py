879f3226931111485e40ecfa7804bbd41be7467c
Roman-Ojha <razzroman98@gmail.com>
2023-05-28 11:42:16 +0545

    feat(backend): added view page for job application detail & approved/disapproved button func
    fixes #8

--- backend/myadmin/views.py
+++ backend/myadmin/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from api.models import JobApplication
+from django.http import HttpResponseRedirect
 
 
 @require_http_methods(["GET"])
 def view_job_application(request, id):
+    try:
+        job_application = JobApplication.objects.get(id=id)
+        return render(request, 'myadmin/view_job_application.html', {'job_application': job_application})
+    except JobApplication.DoesNotExist:
+        return render(request, 'myadmin/view_job_application.html', {'error': 'Provided job application id does not exist'})
+
+
+@require_http_methods(["POST"])
+def approve_job_application(request, id):
     job_application = JobApplication.objects.get(id=id)
-    return render(request, 'myadmin/view_job_application.html', {'job_application': job_application})
+    job_application.is_approved = True
+    job_application.save()
+    return HttpResponseRedirect('/admin/api/jobapplication/')
+
+
+@require_http_methods(["POST"])
+def disapprove_job_application(request, id):
+    job_application = JobApplication.objects.get(id=id)
+    job_application.is_approved = False
+    job_application.save()
+    return HttpResponseRedirect('/admin/api/jobapplication/')
