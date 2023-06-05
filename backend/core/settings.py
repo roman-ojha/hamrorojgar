f61defca6bfa660e44b617a59c21c1bd69ce10a2
Roman-Ojha <razzroman98@gmail.com>
2023-06-05 10:47:36 +0545

    feat: created Citizen model for maindgobdb app

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'api',
     'myadmin',
+    'maingovdb',
 ]
 
 MIDDLEWARE = [
--- backend/maingovdb/models.py
+++ backend/maingovdb/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Citizen(models.Model):
+    f_name = models.CharField(_("First name"), max_length=30)
+    m_name = models.CharField(_("Middle name"), max_length=30, null=True)
+    l_name = models.CharField(_("Last name"), max_length=30)
+    mobile = models.IntegerField()
+    date_of_birth = models.DateField()
+
+    class GenderChoice(models.TextChoices):
+        MALE = 'M', 'Male'
+        FEMALE = 'F', 'Female'
+        OTHERS = 'O', 'Others'
+    gender = models.CharField(
+        max_length=3, choices=GenderChoice.choices)
+    citizenship_no = models.CharField(max_length=200)
+
+    def name(self):
+        if self.m_name:
+            return f"{self.f_name} {self.m_name} {self.l_name}"
+        else:
+            return f"{self.f_name} {self.l_name}"
