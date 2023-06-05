d534d0c627ab4c4587e530a83f4d86ee39e054f6
Roman-Ojha <razzroman98@gmail.com>
2023-06-05 11:03:32 +0545

    feat: registered new model & unregistered Token & Group model for temp purposes

--- backend/api/admin/__init__.py
+++ backend/api/admin/__init__.py
@@ -2,3 +2,10 @@ from .citizen_admin import CitizenUserAdmin
 from .government_admin import GovernmentUserAdmin
 from .job_application_admin import JobApplicationAdmin
 from .vacancy_admin import VacancyAdmin
+
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from rest_framework.authtoken.models import TokenProxy
+
+admin.site.unregister(Group)
+admin.site.unregister(TokenProxy)
--- backend/maingovdb/admin.py
+++ backend/maingovdb/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Citizen
 
-# Register your models here.
+
+@admin.register(Citizen)
+class CitizenAdmin(admin.ModelAdmin):
+    list_display = ['name', 'mobile',
+                    'date_of_birth', 'gender', 'citizenship_no']
