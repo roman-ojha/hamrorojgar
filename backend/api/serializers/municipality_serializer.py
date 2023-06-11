951c9742416ddcc7d068f9893205bbdda80b9d86
Roman-Ojha <razzroman98@gmail.com>
2023-06-11 16:35:55 +0545

    feat(backend): create route, serializer, view to get list of municipalities using district_id
    working on #15

---
+++ backend/api/serializers/municipality_serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from api.models import municipality
+
+
+class MunicipalitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = municipality.Municipality
+        fields = ['id', 'name', 'type']
--- backend/api/views/district_municipality_view.py
+++ backend/api/views/district_municipality_view.py
@@ -4,11 +4,26 @@ from rest_framework.response import Response
 from api.models import district
 from api.serializers import district_serializer
 from rest_framework import status
+from api.models import municipality
+from api.serializers import municipality_serializer
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class District(APIView):
     def get(self, request: Request):
         districts = district.District.objects.all()
-        serialized = district_serializer.DistrictSerializer(
+        serialized_districts = district_serializer.DistrictSerializer(
             districts, many=True)
-        return Response(serialized.data)
+        return Response(serialized_districts.data)
+
+
+class Municipality(APIView):
+    def get(self, request: Request, district_id):
+        try:
+            municipalities = municipality.Municipality.objects.filter(
+                district=district_id)
+            serialized_municipalities = municipality_serializer.MunicipalitySerializer(
+                municipalities, many=True)
+            return Response(serialized_municipalities.data)
+        except ObjectDoesNotExist:
+            return Response([])
