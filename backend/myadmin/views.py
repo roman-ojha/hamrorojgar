663e72fa92bed4bd101ebdaf09ccb68776946d75
Roman-Ojha <razzroman98@gmail.com>
2023-07-13 16:10:48 +0545

    feat(backend): completed email notice for job application approval

--- backend/myadmin/views.py
+++ backend/myadmin/views.py
@@ -4,6 +4,11 @@ from api.models.job_application import JobApplication
 from django.http import HttpResponseRedirect
 from django.http import HttpRequest
 from django.contrib.admin.views.decorators import staff_member_required
+from data.constants import constants
+from utils.email import send_html_email
+from decouple import config
+from api.models import citizen
+from api.models import user
 
 
 @require_http_methods(["GET"])
@@ -30,8 +35,14 @@ def view_job_application(request, id):
 def approve_job_application(request: HttpRequest, id):
     job_application = JobApplication.objects.get(id=id)
     if job_application.vacancy.government.pk == request.user.pk or request.user.is_superuser:
-        job_application.is_approved = True
-        job_application.save()
+        # job_application.is_approved = True
+        # job_application.save()
+        # send main after approval
+        subject = f"{constants.APPLICATION_NAME} - Job application Notice"
+        recipient_list = [
+            job_application.citizen.email]
+        send_html_email(subject, "api/job_application_approval_email.html", {
+                        'application_name': constants.APPLICATION_NAME, 'job_application_title': job_application.vacancy.title, 'client_vacancy_detail_url': f"{config('CLIENT_BASE_URL')}/apply?vacancy_id={job_application.vacancy}"}, ["razzroman98@gmail.com"])
     return HttpResponseRedirect(f"/admin/api/jobapplication/?vacancy_id={job_application.vacancy}")
 
 
